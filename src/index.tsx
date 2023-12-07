0505779e3297efb3cfa9e874283828d41f9ca5cb
zahar <zzzyyy589@gmail.com>
2023-12-07 14:30:53 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RootStoreContext } from "./store/root-store-context";
+import { RootStore } from "./store/root-store";
+import { Test } from "./components/Test";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RootStoreContext.Provider value={new RootStore()}>
+      <RouterProvider router={router} />
+    </RootStoreContext.Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
